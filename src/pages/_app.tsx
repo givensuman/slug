514597e752aba3114232ce812b8110d13acca33f
given <givensuman@gmail.com>
2022-12-04 22:27:28 -0600

    Update description tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return <>
 
     <Head>
-      <meta name="description" content="A small link shortener app" />
+      <meta name="description" content="A tiny link-shortening app" />
       <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🐌</text></svg>" />
     </Head>
 
